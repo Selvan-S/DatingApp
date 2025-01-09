6bc91bdd9ec178b8be9a551d1513ac440491e321
Selvan-S <selvan0023@gmail.com>
2025-01-09 22:32:19 +0530

    <>|users not shown due to not stated the mysql and made the api to async|<selvan>

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,15 +1,30 @@
-using Microsoft.AspNetCore.Mvc
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API;
 
 [ApiController]
 [Route("api/[controller]")]
-public class UsersController : ControllerBase
+public class UsersController(DataContext context) : ControllerBase
 {
-    [HttpGet]
-    public ActionResult<IEnumerable<AppUser>> GetUsers()
+
+    [HttpGet] // GET api/users
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
-        
+        var users = await context.Users.ToListAsync();
+
+        return Ok(users);
     }
 
+    [HttpGet("{id:int}")] // GET api/users/5
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUser(int id)
+    {
+        var user = await context.Users.FindAsync(id);
+
+        if (user == null) return NotFound("User not found");
+
+        return Ok(user);
+    }
 }
