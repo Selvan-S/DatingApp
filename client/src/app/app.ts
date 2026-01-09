dc55ed86494ddc2e2ebed829858bd6663a229c94
Selvan S <87435305+Selvan-S@users.noreply.github.com>
2026-01-09 23:56:05 +0530

    Client login and registration (#13)
    * folder structure and created nav component
    
    * Styling the nav bar
    
    * Using template forms in Angular
    
    * Introduction to Angular services
    
    * Using conditionals in Angular templates
    
    * Persisting the login with signals
    
    * Persisting the login with signals part 2
    
    * Adding a home page
    
    * Adding a register form
    
    * Hooking up the register component to call the AP

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -2,25 +2,38 @@ import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
+import { Nav } from "../layout/nav/nav";
+import { AccountService } from '../core/services/account-service';
+import { Home } from "../features/home/home";
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule],
+  imports: [CommonModule, Nav, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App implements OnInit {
+  private accountService = inject(AccountService);
   private http = inject(HttpClient);
-  protected readonly title = signal('client');
+  protected readonly title = signal('Dating App');
   // set the response user data
   protected users: any = signal<any>([]);
 
   ngOnInit() {
     this.loadUsers();
+    this.setCurrentUser();
   }
 
   private async loadUsers() {
     this.users.set(await this.getUsers());
+  }
+
+  setCurrentUser() {
+    const userJson = localStorage.getItem('user');
+    if (userJson) {
+      const user = JSON.parse(userJson);
+      this.accountService.currentUser.set(user);
+    }
   }
 
   async getUsers() {
