fdfbf2eee423a8034da620ef67a3cd274eb9d48b
Selvan-S <selvan0023@gmail.com>
2026-01-11 23:22:09 +0530

    Adding more components

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,19 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 import { lastValueFrom } from 'rxjs';
-import { Nav } from "../layout/nav/nav";
 import { AccountService } from '../core/services/account-service';
-import { Home } from "../features/home/home";
+import { Nav } from "../layout/nav/nav";
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, Nav, Home],
+  imports: [CommonModule, Nav, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App implements OnInit {
   private accountService = inject(AccountService);
+  protected router = inject(Router)
   private http = inject(HttpClient);
   protected readonly title = signal('Dating App');
   // set the response user data
