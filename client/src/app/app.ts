31bd62e7c1eaa082d7f15bb858569f60b14b5882
Selvan S <87435305+Selvan-S@users.noreply.github.com>
2026-02-15 14:42:42 +0530

    Routing fe (#14)
    * Adding more components
    
    * Adding nav links
    
    * 1. Routing Management: The lecture discusses how to manage
    routing within a component using a TypeScript file named nav.ts .
    2. Injecting the Router: The speaker demonstrates how to inject the
    router into the component to enhance encapsulation.
    3.Redirection After Authentication:
     - Upon successful login, users are redirected to the
    '/members' page using the router's navigateByUr1 function.
     - Users are routed back to the home component when they log
    out.
    4. TypeScript Syntax: The optional use of semicolons in TypeScript
    is discussed, with the speaker expressing a preference for
    including them for clarity.
    5. Demonstration: A test account is used to showcase the
    functionality, confirming that redirection occurs as intended.
    6. User Experience: The lecture concludes with a note on adding
    notification toasts to enhance user experience, which will be
    explored in the next lesson.
    
    * Adding toasts
    
    * update
    
    * toast implementation
    
    * Adding a route guard
    
    * Adding app initialization
    
    * Adding route animations

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,49 +1,16 @@
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { lastValueFrom } from 'rxjs';
+import { Component, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 import { Nav } from "../layout/nav/nav";
-import { AccountService } from '../core/services/account-service';
-import { Home } from "../features/home/home";
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, Nav, Home],
+  imports: [CommonModule, Nav, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements OnInit {
-  private accountService = inject(AccountService);
-  private http = inject(HttpClient);
-  protected readonly title = signal('Dating App');
-  // set the response user data
-  protected users: any = signal<any>([]);
-
-  ngOnInit() {
-    this.loadUsers();
-    this.setCurrentUser();
-  }
-
-  private async loadUsers() {
-    this.users.set(await this.getUsers());
-  }
-
-  setCurrentUser() {
-    const userJson = localStorage.getItem('user');
-    if (userJson) {
-      const user = JSON.parse(userJson);
-      this.accountService.currentUser.set(user);
-    }
-  }
-
-  async getUsers() {
-    try {
-      return lastValueFrom(this.http.get<string>('https://localhost:5001/api/users'));
-    } catch (error) {
-      console.error('Error fetching users:', error);
-      return [];
-    }
-  }
+export class App {
+  protected router = inject(Router);
 }
 
 
