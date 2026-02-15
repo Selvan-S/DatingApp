c735231d0c1ff1adc1db2748c7379dd32144ee22
Selvan-S <selvan0023@gmail.com>
2026-02-15 14:40:42 +0530

    Adding route animations

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,9 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
-import { lastValueFrom } from 'rxjs';
-import { AccountService } from '../core/services/account-service';
 import { Nav } from "../layout/nav/nav";
 
 @Component({
@@ -12,39 +9,8 @@ import { Nav } from "../layout/nav/nav";
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements OnInit {
-  private accountService = inject(AccountService);
-  protected router = inject(Router)
-  private http = inject(HttpClient);
-  protected readonly title = signal('Dating App');
-  // set the response user data
-  protected users: any = signal<any>([]);
-
-  ngOnInit() {
-    this.loadUsers();
-    this.setCurrentUser();
-  }
-
-  private async loadUsers() {
-    this.users.set(await this.getUsers());
-  }
-
-  setCurrentUser() {
-    const userJson = localStorage.getItem('user');
-    if (userJson) {
-      const user = JSON.parse(userJson);
-      this.accountService.currentUser.set(user);
-    }
-  }
-
-  async getUsers() {
-    try {
-      return lastValueFrom(this.http.get<string>('https://localhost:5001/api/users'));
-    } catch (error) {
-      console.error('Error fetching users:', error);
-      return [];
-    }
-  }
+export class App {
+  protected router = inject(Router);
 }
 
 
