ab0fc85dea27b0a1d21147fa870070308fce10f0
Selvan-S <selvan0023@gmail.com>
2026-02-19 00:32:16 +0530

    Seeding data part one

--- API/DTOs/SeedUserDto.cs.cs
+++ API/DTOs/SeedUserDto.cs.cs
@@ -1,25 +1,18 @@
 using System;
-using System.ComponentModel.DataAnnotations.Schema;
 
-namespace API.Entities;
+namespace API.DTOs;
 
-public class Member
+public class SeedUserDto
 {
     public string Id { get; set; } = null!;
+    public required string Email { get; set; }
     public DateOnly DateOfBirth { get; set; }
     public string? ImageUrl { get; set; }
     public required string DisplayName { get; set; }
-    public DateTime Created { get; set; } = DateTime.UtcNow;
-    public DateTime LastActive { get; set; } = DateTime.UtcNow;
+    public DateTime Created { get; set; }
+    public DateTime LastActive { get; set; }
     public required string Gender { get; set; }
     public string? Description { get; set; }
     public required string City { get; set; }
     public required string Country { get; set; }
-
-    // Navigation properties
-    public List<Photo> Photos { get; set; } = [];
-
-    [ForeignKey(nameof(Id))]
-    public AppUser User { get; set; } = null!;
-
 }
--- API/Entities/Photo.cs
+++ API/Entities/Photo.cs
@@ -10,4 +10,5 @@ public class Photo
 
     // Navigation properties
     public Member Member { get; set; } = null!;
+    public string MemberId { get; set; } = null!;
 }
