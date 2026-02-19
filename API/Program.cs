e76eea2b979e6860b451770ed18988f2933f815f
Selvan-S <selvan0023@gmail.com>
2026-02-19 22:23:04 +0530

    Implement database migration and seeding in Program.cs; move HMACSHA512 instantiation in Seed.cs

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API;
+using API.Data;
 using API.Interfaces;
 using API.Middleware;
 using API.Services;
@@ -41,5 +42,19 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration or seeding.");
+}
 
 app.Run();
