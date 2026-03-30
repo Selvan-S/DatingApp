a1e3709cbca7f66a2b1c81914b6453ed099e3da8
Selvan-S <selvan0023@gmail.com>
2026-03-30 22:49:13 +0530

    Using the service to store the signal

--- client/src/features/members/member-profile/member-profile.ts
+++ client/src/features/members/member-profile/member-profile.ts
@@ -22,8 +22,6 @@ export class MemberProfile implements OnInit, OnDestroy {
   }
   protected memberService = inject(MemberService);
   private toast = inject(ToastService);
-  private route = inject(ActivatedRoute);
-  protected member = signal<Member | undefined>(undefined);
   protected editableMember: EditableMember = {
     displayName: '',
     description: '',
@@ -35,28 +33,25 @@ export class MemberProfile implements OnInit, OnDestroy {
 
   }
   ngOnInit(): void {
-    this.route.parent?.data.subscribe((data) => {
-      this.member.set(data['member']);
-    });
-
     this.editableMember = {
-      displayName: this.member()?.displayName || '',
-      description: this.member()?.description || '',
-      city: this.member()?.city || '',
-      country: this.member()?.country || '',
+      displayName: this.memberService.member()?.displayName || '',
+      description: this.memberService.member()?.description || '',
+      city: this.memberService.member()?.city || '',
+      country: this.memberService.member()?.country || '',
     }
   }
 
   updateProfile() {
-    if (!this.member) return;
+    if (!this.memberService.member) return;
     const updatedMember = {
-      ...this.member(),
+      ...this.memberService.member(),
       ...this.editableMember
     }
     this.memberService.UpdateMember(this.editableMember).subscribe({
       next: () => {
         this.toast.success('Profile updated successfully');
         this.memberService.editMode.set(false);
+        this.memberService.member.set(updatedMember as Member);
         this.editForm?.reset(updatedMember);
       }
     })
