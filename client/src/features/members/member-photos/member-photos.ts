c232804dfdeb75fc11b3f37813e669bd807fb723
Selvan-S <selvan0023@gmail.com>
2026-04-01 23:26:54 +0530

    Setting the main image on the client UI. with reusable component implementation for the start-button

--- client/src/features/members/member-photos/member-photos.ts
+++ client/src/features/members/member-photos/member-photos.ts
@@ -3,16 +3,21 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MemberService } from '../../../core/services/member-service';
 import { ImageUpload } from "../../../shared/image-upload/image-upload";
-import { Photo } from '../../../types/member';
+import { Member, Photo } from '../../../types/member';
+import { User } from '../../../types/user';
+import { AccountService } from '../../../core/services/account-service';
+import { MainPhotoButton } from "../../../shared/main-photo-button/main-photo-button";
+import { StarButton } from "../../../shared/star-button/star-button";
 
 @Component({
   selector: 'app-member-photos',
-  imports: [ImageUpload],
+  imports: [ImageUpload, MainPhotoButton, StarButton],
   templateUrl: './member-photos.html',
   styleUrl: './member-photos.css',
 })
 export class MemberPhotos implements OnInit {
   protected memberSevice = inject(MemberService);
+  private accountService = inject(AccountService);
   private route = inject(ActivatedRoute);
   protected photos = signal<Photo[]>([]);
   protected loading = signal(false);
@@ -41,4 +46,19 @@ export class MemberPhotos implements OnInit {
       }
     });
   }
+
+  setMainPhoto(photo: Photo) {
+    this.memberSevice.setMainPhoto(photo).subscribe({
+      next: () => {
+        const currentMember = this.accountService.currentUser();
+        if (currentMember) currentMember.imageUrl = photo.url;
+        this.accountService.setCurrentUser(currentMember as User);
+        this.memberSevice.member.update(member => ({
+          ...member,
+          imageUrl: photo.url
+        }) as Member);
+      }
+    });
+  }
+
 }
