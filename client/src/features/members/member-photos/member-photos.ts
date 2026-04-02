930eb1756c72300bae0616c904983f69d1c5d5fb
Selvan-S <selvan0023@gmail.com>
2026-04-02 22:43:30 +0530

    Deleting a photo - client

--- client/src/features/members/member-photos/member-photos.ts
+++ client/src/features/members/member-photos/member-photos.ts
@@ -1,23 +1,22 @@
-import { AsyncPipe } from '@angular/common';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AccountService } from '../../../core/services/account-service';
 import { MemberService } from '../../../core/services/member-service';
+import { DeleteButton } from "../../../shared/delete-button/delete-button";
 import { ImageUpload } from "../../../shared/image-upload/image-upload";
+import { StarButton } from "../../../shared/star-button/star-button";
 import { Member, Photo } from '../../../types/member';
 import { User } from '../../../types/user';
-import { AccountService } from '../../../core/services/account-service';
-import { MainPhotoButton } from "../../../shared/main-photo-button/main-photo-button";
-import { StarButton } from "../../../shared/star-button/star-button";
 
 @Component({
   selector: 'app-member-photos',
-  imports: [ImageUpload, MainPhotoButton, StarButton],
+  imports: [ImageUpload, StarButton, DeleteButton],
   templateUrl: './member-photos.html',
   styleUrl: './member-photos.css',
 })
 export class MemberPhotos implements OnInit {
   protected memberSevice = inject(MemberService);
-  private accountService = inject(AccountService);
+  protected accountService = inject(AccountService);
   private route = inject(ActivatedRoute);
   protected photos = signal<Photo[]>([]);
   protected loading = signal(false);
@@ -58,6 +57,15 @@ export class MemberPhotos implements OnInit {
           imageUrl: photo.url
         }) as Member);
       }
+    });
+  }
+
+  deletePhoto(photoId: number) {
+    this.memberSevice.deletePhoto(photoId).subscribe({
+      next: () => {
+        this.photos.update(photos => photos.filter(p => p.id !== photoId));
+      },
+      error: err => console.error('Error deleting photo:', err)
     });
   }
 
