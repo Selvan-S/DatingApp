7b689671a6e431bfebd340afaad8c55d1f238900
Selvan-S <selvan0023@gmail.com>
2026-04-07 22:43:26 +0530

    Adding custom password validation

--- client/src/features/account/register/register.ts
+++ client/src/features/account/register/register.ts
@@ -1,8 +1,9 @@
 import { JsonPipe } from '@angular/common';
 import { Component, inject, OnInit, output } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { AccountService } from '../../../core/services/account-service';
 import { RegisterCreds } from '../../../types/user';
+import { ValidationError } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-register',
@@ -25,8 +26,22 @@ export class Register implements OnInit {
       email: new FormControl('', [Validators.required, Validators.email]),
       displayName: new FormControl('', Validators.required),
       password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
-      confirmPassword: new FormControl('', Validators.required),
-    })
+      confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')]),
+    });
+
+    this.registerForm.controls['password'].valueChanges.subscribe(() => {
+      this.registerForm.controls['confirmPassword'].updateValueAndValidity();
+    });
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const parent = control.parent as FormGroup;
+      if (!parent) return null;
+
+      const matchValue = parent.get(matchTo)?.value;
+      return control.value === matchValue ? null : { passwordMismatch: true }
+    }
   }
 
   protected register() {
