f312b48fd01a682b2f08d14140b5ef65513ca1ef
Selvan-S <selvan0023@gmail.com>
2026-04-07 22:29:17 +0530

    Client side validation

--- client/src/features/account/register/register.ts
+++ client/src/features/account/register/register.ts
@@ -1,6 +1,6 @@
 import { JsonPipe } from '@angular/common';
 import { Component, inject, OnInit, output } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AccountService } from '../../../core/services/account-service';
 import { RegisterCreds } from '../../../types/user';
 
@@ -22,10 +22,10 @@ export class Register implements OnInit {
 
   initializeForm() {
     this.registerForm = new FormGroup({
-      email: new FormControl(),
-      displayName: new FormControl(),
-      password: new FormControl(),
-      confirmPassword: new FormControl(),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      displayName: new FormControl('', Validators.required),
+      password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+      confirmPassword: new FormControl('', Validators.required),
     })
   }
 
--- client/src/features/home/home.ts
+++ client/src/features/home/home.ts
@@ -8,7 +8,7 @@ import { Register } from "../account/register/register";
   styleUrl: './home.css',
 })
 export class Home {
-  protected registerMode = signal(false);
+  protected registerMode = signal(true);
   
   showRegisterMode(value: boolean) {
     this.registerMode.set(value);
